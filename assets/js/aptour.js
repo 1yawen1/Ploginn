b889fac07b4698d4d7b59d884bc67127a8cb8395
YA WEN CHANG <103559970+1yawen1@users.noreply.github.com>
2023-02-05 22:49:29 +0800

    Update aptour2023.02.05

--- assets/js/aptour.js
+++ assets/js/aptour.js
@@ -155,7 +155,7 @@ APTour.prototype.handleClicks = function(event) {
 			case 'close' : 
 				document.body.removeChild(this.overlay);
 				document.body.removeChild(this.tourWindow);
-				window.location.href = "https://1yawen1.github.io/Ploginn/convert1";
+				window.location.href = "http://127.0.0.1:5500/convert1.html";
 
 			break;
 		}
